c30c6d02c7053a629686fe4ad9eecbf05dcbbf1f
chayilDeborah <aniyikayeolamide3@gmail.com>
2024-08-16 18:32:50 +0100

    feat: complete FAQ component

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,17 @@
 import React from 'react'
 import Homepage from './component/Homepage'
 import About from './component/About'
+import Utilities from './component/FAQ'
 
 const Home = () => {
   return (
     <main className='w-full  mx-auto overflow-hidden  '>
-        <section className='image w-full lg:mt-[0px] lg:h-[1500px]'>
+        <section className='image w-full lg:mt-[0px] lg:h-auto'>
           <div className='content'>
             <section className='lg:max-w-[1440px] w-[95%] mx-auto lg:w-full'>
            <Homepage />
            <About />
+           <Utilities />
             </section>
           </div>
 
